6192f4771b3a7f2f2d09d9538c1a40c5cac9bc2e
sridevijagadesh <sridevi.j2719@gmail.com>
2024-06-11 10:03:16 +0530

    home page

--- src/Component/Navbar/Navbar.jsx
+++ src/Component/Navbar/Navbar.jsx
@@ -1,18 +1,20 @@
-import React from 'react'
-import {Link} from 'react-router-dom'
+import React, {  } from 'react'
+import {NavLink} from 'react-router-dom'
 import '../Navbar/Navbar.css'
 
 const Navbar = () => {
+  // const [nav , setNav] =useState('')
+ 
   return (
     <div className='navbar'>
       <div className="nav-log">
         <p>AT-IT-HUB</p>
       </div>
       <ul className='nav-menu'>
-        <li><Link to='/' className='link active'>Home</Link></li>
-        <li><Link to='/about'  className='link'>About</Link></li>
-        <li><Link to='/service'  className='link'>service</Link></li>
-        <li><Link to='/contact' className='link'>contact</Link></li>
+        <li ><NavLink to='/' className='link ' >Home</NavLink></li>
+        <li><NavLink to='/about'  className='link'>About</NavLink></li>
+        <li><NavLink to='/service'  className='link'>service</NavLink></li>
+        <li><NavLink to='/contact' className='link'>contact</NavLink></li>
       </ul>
     </div>
   )
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,10 +1,36 @@
 import React from 'react'
+import '../Pages/CSS/home.css'
+import home_img from '../Component/assest/Home.png.png'
 
 const Home = () => {
   return (
-    <div>
-      home
+    <>
+    <div className='home'>
+     <h1>Welcome To AK IT PVT LTD</h1>
+      <button className='button'>Explore project</button>
+      <div className='home-img'>
+        <img src={home_img}  alt=''/>
+  </div>
     </div>
+   
+    <div className='about'>
+         <div className='about-container'>
+          <h1>ABOUT US</h1>
+          </div>
+          <div className='about-web'>
+            <div className='about-web1'>
+            
+            <h1>Welcome to our website</h1>
+            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ullam suscipit quidem quos temporibus illo hic. Magni, sunt deserunt! Expedita iure recusandae, eveniet magni voluptates aliquam fugit odit autem repellat. Vitae!</p>
+           </div>
+            <div className='about-web'>
+              <img src='https://i.pinimg.com/originals/af/7b/7a/af7b7a8ee3766dcf960c3c2f9d6010ac.jpg' alt=''/>
+            </div>
+            </div>
+      
+          
+    </div>
+    </>
   )
 }
 

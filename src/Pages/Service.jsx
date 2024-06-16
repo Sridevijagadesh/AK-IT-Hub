628de6f64b0f5e9b315694508433a55147604863
sridevijagadesh <sridevi.j2719@gmail.com>
2024-06-16 13:57:23 +0530

    sevicepage new

--- src/Pages/Service.jsx
+++ src/Pages/Service.jsx
@@ -1,7 +1,53 @@
 import React from 'react'
-
+import '../Pages/CSS/service.css'
+import service_img from '../Component/assest/pngwing.com.png'
 const Service = () => {
   return (
+    <>
+    <div className='service-banner'>
+      <div className="service-banner1">
+   <h1>Web Design Service</h1>
+            <button>Get My Free Proposal</button>
+            </div>
+  
+    </div>
+    <div className='service-content'>
+    <p>The internet is a goldmine of opportunities for all kinds of businesses worldwide. Leverage its power to your advantage. Establish your digital presence today and discover your market potential with Thrive Internet Marketing Agency’s website design services. From website design and development to search engine optimization (SEO) and site maintenance, we’ve got you covered!
+      <h1>Superior Web Design Represents Your Company and Grows
+      Your Business</h1>
+    </p>
+    </div>
+    <div className='service-row'>
+      <div className="service-row1">
+    <p>Creating a digital presence doesn’t mean simply building
+                        a website and waiting for customers to approach your
+                        brand. Your website is your company’s online foundation.
+                        It serves as your primary customer touchpoint and
+                        conversion machine. As such, you must ensure it ranks
+                        high in search engines, stands out from the competition
+                        and relates to your visitors’ intentions.</p>
+                    <p>Statistics reveal that website design and navigation
+                        influence 94 percent of first impressions. Search
+                        engines also favor websites with responsive web design
+                        and well-structured web content. What’s more, 75 percent
+                        of site credibility comes from web page design.</p>
+                    <p>As a business owner, it is critical that you maintain a
+                        responsive website design to appeal to search engines
+                        and online users. You must ensure your target customers
+                        see a legit business platform when they search for your
+                        company online. Otherwise, you risk losing trust from
+                        your prospective clients and dissuade them from engaging
+                        with your brand.</p>
+                    <p>Invest in responsive website design and build trust and
+                        confidence in your brand offerings. Our web design
+                        company provides SEO-friendly website design services
+                        that aim to boost your search rankings, increase your
+                        online exposure and convert page visitors into paying
+                        customers.</p>
+                        </div>
+                        <img src={service_img}alt=''/>
+
+    </div>
     <div className='service'>
     <div className='service-header'>
       <h1>Service provided</h1>
@@ -38,6 +84,7 @@ const Service = () => {
         
     </div>
    </div>
+   </>
   )
 }
 

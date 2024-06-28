605cad70f0256978cd28396d6aab77403a551886
sridevijagadesh <sridevi.j2719@gmail.com>
2024-06-28 11:18:40 +0530

    customer

--- src/Admin/component/Customer.jsx
+++ src/Admin/component/Customer.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'
+ import React, { useEffect, useState } from 'react'
 import Axios from 'axios'
 import '../CSS/customer.css'
 import { FaX } from 'react-icons/fa6'
